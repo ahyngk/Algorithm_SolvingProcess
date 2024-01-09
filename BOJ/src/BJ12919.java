849d53c23dfb658632a7c44c42717d9a8b2b26fc
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-01-10 00:16:50 +0900

    [Solved] BJ12919

--- BOJ/src/BJ12919.java
+++ BOJ/src/BJ12919.java
@@ -1,30 +1,79 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BJ12919 {
-    static long original = 0;
-    static long toChange = 0;
+    static String original;
+    static String reverseOriginal;
+    static List<String> toChange;
+    static int maxChange = 0;
+    static boolean check = false;
     public static void main(String[] args) throws IOException {
         read();
-        System.out.println("original = " + original);
-        System.out.println("toChange = " + toChange);
+        select(0,new int[maxChange]);
+        System.out.println(check == true ? 1: 0);
     }
     static void read() throws IOException {
+        // A is 0, B is 1
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String origin = br.readLine();
-        for (int i = origin.length()-1; i >= 0; i--) {
-            if(origin.charAt(i) == 'B'){
-                original += Math.pow(2,(origin.length()-1-i));
-            }
-        }
-        String change = br.readLine();
-        for (int i = change.length()-1; i >= 0; i--) {
-            if(change.charAt(i) == 'B'){
-                toChange += Math.pow(2,(change.length()-1-i));
-            }
-        }
+        toChange = Arrays.stream(br.readLine().split("")).collect(Collectors.toList());
+        original = br.readLine();
+        reverseOriginal = new StringBuilder(original).reverse().toString();
+        maxChange = original.length()-toChange.size();
     }
 
-
+    static void select(int index, int[] selected){
+        if(check){
+            return;
+        }
+        if(index == selected.length){
+            runSelected(selected);
+            return;
+        }
+        for (int i = 0; i < 2; i++) {
+            selected[index] = i;
+            select(index+1, selected);
+        }
+    }
+    static void runSelected(int[] selected){
+        List<String> clone = new ArrayList<>(toChange);
+        int currentIndex = clone.size();
+        for (int i = 0; i < selected.length; i++) {
+            switch (selected[i]){
+                case 0 :
+                    clone.add(currentIndex,"A");
+                    if(currentIndex != 0){
+                        currentIndex = clone.size();
+                    }
+                    break;
+                case 1:
+                    clone.add(currentIndex,"B");
+                    currentIndex = reverse(currentIndex, clone);
+                    break;
+            }
+        }
+        checkSame(clone, currentIndex);
+    }
+    static int reverse(int currentIndex, List<String> list){
+        return currentIndex == 0 ? list.size() : 0;
+    }
+    static void checkSame(List<String> list, int currentIndex){
+        StringBuilder sb = new StringBuilder();
+        for (String s : list){
+            sb.append(s);
+        }
+        String fin = sb.toString();
+        if(fin.equals(original)){
+            check = true;
+        }
+        else if(fin.equals(reverseOriginal)) {
+            check = true;
+        }
+    }
 }
---
+++ BOJ/src/BJ12919_01.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ12919_01 {
+    static String start;
+    static String des;
+    static boolean check = false;
+    public static void main(String[] args) throws IOException{
+        read();
+        select(0,0,des.length(), false);
+        System.out.println(check == true ? 1 : 0);
+    }
+     static void read() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        start = br.readLine();
+        des = br.readLine();
+    }
+    // 초기 접근은 선택을 완료한 이후 가능한 선택지인지 체크하는 방식으로 진행 -> 2^50가지를 모두 확인하는 것은 동일하므로 시간초과
+    // 재귀 선택 과정에서 선택이 불가능한 선택지의 경우 진행하지 않는 방식으로 변경 필요
+    static void select(int index, int startIndex, int lastIndex, boolean isReversed){
+        if(check){
+            return;
+        }
+        if(index == des.length() - start.length()){
+            checkSame(des.substring(startIndex,lastIndex), isReversed);
+            return;
+        }
+
+        if(checkA(startIndex,lastIndex,isReversed)){
+            int nextstartIndex = isReversed ? startIndex+1 : startIndex;
+            int nextlastIndex = isReversed ? lastIndex : lastIndex-1;
+            select(index+1, nextstartIndex, nextlastIndex, isReversed);
+        }
+        if (checkB(startIndex, lastIndex,isReversed)) {
+            int nextstartIndex = isReversed ? startIndex : startIndex+1;
+            int nextlastIndex = isReversed ? lastIndex-1 : lastIndex;
+            select(index+1, nextstartIndex, nextlastIndex, !isReversed);
+        }
+    }
+    static boolean checkA(int startIndex, int lastIndex, boolean isReversed){
+        if(!isReversed && des.charAt(lastIndex-1) == 'A'){
+            return true;
+        }
+        else if (isReversed && des.charAt(startIndex) == 'A'){
+            return true;
+        }
+        return false;
+    }
+    static boolean checkB(int startIndex, int lastIndex, boolean isReversed){
+        if(!isReversed  && des.charAt(startIndex) == 'B'){
+            return true;
+        }
+        else if (isReversed  && des.charAt(lastIndex-1) == 'B'){
+            return true;
+        }
+        return false;
+    }
+    static void checkSame(String s, boolean isReversed){
+        if(isReversed){
+            s = new StringBuilder(s).reverse().toString();
+        }
+        if(s.equals(start)){
+            check = true;
+        }
+    }
+}
