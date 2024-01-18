d13e477a4c57b8bbda96e80c1e1458216deb9381
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-01-18 22:32:30 +0900

    [Solved] BJ2174

--- BOJ/src/BJ2174.java
+++ BOJ/src/BJ2174.java
@@ -9,9 +9,10 @@ import java.util.StringTokenizer;
 public class BJ2174 {
     static Robot[][] map;
     static List<Command> commandList;
+    static List<Robot> robotList;
     public static void main(String[] args) throws IOException {
         read();
-        printMap();
+        System.out.println(run());
     }
     private static void read() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,47 +24,80 @@ public class BJ2174 {
         st = new StringTokenizer(br.readLine());
         int robotSize = Integer.parseInt(st.nextToken());
         int commandSize = Integer.parseInt(st.nextToken());
+        robotList = new ArrayList<>();
         for (int i = 0; i < robotSize; i++) {
             st = new StringTokenizer(br.readLine());
             Robot robot = new Robot(i+1,Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1, st.nextToken());
             map[robot.x][robot.y] = robot;
+            robotList.add(robot);
         }
 
         commandList = new ArrayList<>();
         for (int i = 0; i < commandSize; i++) {
             st = new StringTokenizer(br.readLine());
-            commandList.add(new Command(Integer.parseInt(st.nextToken()), st.nextToken(),Integer.parseInt(st.nextToken())));
+            commandList.add(new Command(Integer.parseInt(st.nextToken())-1, st.nextToken(),Integer.parseInt(st.nextToken())));
         }
     }
-    static void run(){
-
+    static String run(){
+        for (int i = 0; i < commandList.size(); i++) {
+            Robot robot = robotList.get(commandList.get(i).robotNum);
+            switch (commandList.get(i).commandKind){
+                case "L" :
+                    commandL(robot,commandList.get(i).repeatSize);
+                    break;
+                case "R" :
+                    commandR(robot,commandList.get(i).repeatSize);
+                    break;
+                case "F" :
+                    String result = commandF(robot,commandList.get(i).repeatSize);
+                    if(!result.equals("OK")){
+                        return result;
+                    }
+                    break;
+            }
+        }
+        return "OK";
     }
-    static boolean commandLR(Robot robot, int direction){
+    static int configureDirection(String direction){
+        switch (direction){
+            case "N" : return 0;
+            case "E" : return 1;
+            case "S" : return 2;
+            case "W" : return 3;
+        }
+        return -1;
+    }
+    static void commandL(Robot robot, int repeat){
         // Turn Left 90 degrees
         String[] directions = {"W", "N","E","S"};
-        robot.direction = directions[direction];
-        map[robot.x][robot.y] = robot;
-        return true;
+        for (int i = 0; i < repeat; i++) {
+            robot.direction = directions[configureDirection(robot.direction)];
+        }
     }
-    static boolean commandR(Robot robot, int direction){
+    static void commandR(Robot robot, int repeat){
         // Turn Right 90 degrees
         String[] directions = {"E","S","W","N"};
-        robot.direction = directions[direction];
-        map[robot.x][robot.y] = robot;
-        return true;
-    }
-    static boolean commandF(Robot robot, int direction){
-        int[][] directions = {{1,0},{0,-1},{0,1},{-1,0}};
-        int nextX = robot.x + directions[direction][0];
-        int nextY = robot.y + directions[direction][1];
-        if(checkBorder(nextX,nextY)){
-            return false;
+        for (int i = 0; i < repeat; i++) {
+            robot.direction = directions[configureDirection(robot.direction)];
         }
-        map[robot.x][robot.y] = null;
-        robot.x = nextX;
-        robot.y = nextY;
-        map[robot.x][robot.y] = robot;
-        return true;
+    }
+    static String commandF(Robot robot, int repeat){
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        for (int i = 0; i < repeat; i++) {
+            int nextX = robot.x + directions[configureDirection(robot.direction)][0];
+            int nextY = robot.y + directions[configureDirection(robot.direction)][1];
+            if(!checkBorder(nextX,nextY)){
+                return makePrintString(robot.robotNum);
+            }
+            if(map[nextX][nextY] != null){
+                return makePrintString(robot.robotNum, map[nextX][nextY].robotNum);
+            }
+            map[robot.x][robot.y] = null;
+            robot.x = nextX;
+            robot.y = nextY;
+            map[robot.x][robot.y] = robot;
+        }
+        return "OK";
     }
     static boolean checkBorder(int x, int y){
         if(x>=0 && x< map.length && y>=0 && y<map[0].length){
@@ -71,9 +105,18 @@ public class BJ2174 {
         }
         return false;
     }
+    static String makePrintString(int robotNum){
+        return "Robot "+robotNum+" crashes into the wall";
+    }
+    static String makePrintString(int robot1, int robot2){
+        return "Robot "+robot1+" crashes into robot "+robot2;
+    }
     static void printMap(){
-        for (int i = map.length-1; i >= 0; i--) {
-            System.out.println(Arrays.toString(map[i]));
+        for (int j = map[0].length-1; j >= 0; j--) {
+            for (int i = 0; i < map.length; i++) {
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
         }
     }
     static class Robot {
@@ -93,18 +136,12 @@ public class BJ2174 {
 
         @Override
         public String toString() {
-            return "Robot{" +
-                    "robotNum=" + robotNum +
-                    ", x=" + x +
-                    ", y=" + y +
-                    ", direction='" + direction + '\'' +
-                    '}';
+            return robotNum + direction;
         }
     }
     static class Command {
         public Command() {
         }
-
         public Command(int robotNum, String commandKind, int repeatSize) {
             this.robotNum = robotNum;
             this.commandKind = commandKind;
